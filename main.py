25a741a2ee91bf6a56e1ce0a449dc551634d3228
Ivan Zyulin <solar.ivz@gmail.com>
2024-06-03 00:24:30 +1200

    add ex 2
    Задание 2: Параметры запрос

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import pprint
 
+print("*"*100)
+print("Задание 1: Получение данных")
 # Задание 1: Получение данных
 # Импортируйте библиотеку requests.
 # Отправьте GET-запрос к открытому API (например, https://api.github.com) с
@@ -18,3 +20,19 @@ print(response.ok)
 response_json = response.json()
 pprint.pprint(response_json)
 
+print("*"*100)
+print("Задание 2: Параметры запрос")
+# Задание 2: Параметры запрос
+# 1. Используйте API, который позволяет фильтрацию данных через
+# URL-параметры (например, https://jsonplaceholder.typicode.com/posts).
+#
+# 2. Отправьте GET-запрос с параметром `userId`, равным `1`.
+#
+# 3. Распечатайте полученные записи.
+
+
+url = "https://jsonplaceholder.typicode.com/posts"
+data = {"userId" : 1}
+response = requests.post(url, data=data)
+print(response.status_code)
+print(f"ответ- {response.json()}")
